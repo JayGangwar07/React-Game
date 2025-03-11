3809e42154151a04b3f7a2a8b732c42b772d4b54
JayGangwar07 <jaygangwar86@gmail.com>
2025-03-11 10:28:33 +0530

    Update Start.jsx

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router'
+import backPikachu from "../assets/backPikachu.png"
 
 const Start = () => {
   const [selectedPokemon, setSelectedPokemon] = useState(null);
